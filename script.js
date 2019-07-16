c2943b90173e5610e43860f9dd278f41d58f91d6
Michael Grein <michaelgrein92@gmail.com>
2019-07-16 16:15:56 -0400

    Created profile and image endpoints

--- script.js
+++ script.js
@@ -29,6 +29,22 @@ app.get('/', (req, res) => {
 	res.send(database.users);
 });
 
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+
+	database.users.forEach( user => {
+		if (user.id === id) {
+			found = true;
+			res.json(user);
+		}
+	});
+
+	if (!found) {
+		res.status(400).json('No user found');
+	}
+});
+
 app.post('/signin', (req, res) => {
 	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
 		res.json('signed in');
@@ -36,7 +52,7 @@ app.post('/signin', (req, res) => {
 		res.status(400).json('error logging in');
 	}
 	res.json('Sign In');
-})
+});
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
@@ -52,6 +68,23 @@ app.post('/register', (req, res) => {
 	);
 	res.json(database.users[database.users.length - 1]);
 })
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+
+	database.users.forEach( user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		}
+	});
+
+	if (!found) {
+		res.status(400).json('No user found');
+	}
+});
 
 app.listen(3000, () => {
 	console.log('bitch please');
