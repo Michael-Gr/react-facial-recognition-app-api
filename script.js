74bd0c33df9b4a0985a93d11d52f1e387b69a054
Michael Grein <michaelgrein92@gmail.com>
2019-07-17 19:53:57 -0400

    removed unused endpoint for images

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ app.post('/signin', (req, res) => {
 	} else {
 		res.status(400).json('error logging in');
 	}
+	res.json(database.users[0]);
 });
 
 app.post('/register', (req, res) => {
@@ -76,23 +77,6 @@ app.post('/register', (req, res) => {
 	);
 	res.json(database.users[database.users.length - 1]);
 })
-
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	let found = false;
-
-	database.users.forEach( user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	});
-
-	if (!found) {
-		res.status(400).json('No user found');
-	}
-});
 
 app.listen(3000, () => {
 	console.log('API listening to port 3000');
